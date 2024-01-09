6d4d7942f3ee6578677a6312749e9bf4d9702441
Aniket Singh <asingh11771@gmail.com>
2024-01-09 14:14:22 -0600

    feat: added sys and file functionality

--- Password-Checker/main.py
+++ Password-Checker/main.py
@@ -2,13 +2,19 @@ import password_checker
 import sys
 
 if __name__ == '__main__':
-    passwords = sys.argv[1:]
+    sys_passwords = sys.argv[1:]
 
-    for password in passwords:
-        response, tail = password_checker.password_check(password)
-        count = password_checker.get_password_leaks(response, tail)
+    if len(sys_passwords) == 0:
+        with open("Passwords.txt", "r") as file:
+            passwords = file.read().splitlines()
+    else:
+        passwords = sys_passwords
 
-        if count == 0:
-            print(f"{password} has not been breached")
-        else:
-            print(f"{password} has been breached {count} times")
+for password in passwords:
+    response, tail = password_checker.password_check(password)
+    count = password_checker.get_password_leaks(response, tail)
+
+    if count == 0:
+        print(f"{password} has not been breached")
+    else:
+        print(f"{password} has been breached {count} times")
