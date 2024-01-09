11e83d34f3887e20f59632e4fa353129bdbbd8d0
Aniket Singh <asingh11771@gmail.com>
2024-01-09 15:12:08 -0600

    chore: added comments

--- Password-Checker/main.py
+++ Password-Checker/main.py
@@ -2,11 +2,16 @@ import password_checker
 import sys
 
 if __name__ == '__main__':
+    # Checks if any arguments have been given through Command Line / Terminal
     sys_passwords = sys.argv[1:]
 
+    # If there are no sys arguments, check Passwords.txt
     if len(sys_passwords) == 0:
         with open("Passwords.txt", "r") as file:
             passwords = file.read().splitlines()
+
+            if len(passwords) == 0:
+                print("No passwords given through command line or file")
     else:
         passwords = sys_passwords
 
--- Password-Checker/password_checker.py
+++ Password-Checker/password_checker.py
@@ -2,24 +2,31 @@ import requests
 import hashlib
 
 
+# Requests Pwned Password Information
 def request_api(query):
-    url = "https://api.pwnedpasswords.com/range/" + query
-    response = requests.get(url)
+    _url = "https://api.pwnedpasswords.com/range/" + query
+    response = requests.get(_url)
+
+    # Breaks if API call fails
     if response.status_code != 200:
-        raise RuntimeError(f'Request to {url} failed. Status code: {response.status_code}')
+        raise RuntimeError(f'Request to {_url} failed. Status code: {response.status_code}')
 
     return response
 
 
 def password_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+
+    # Takes only first 5 characters as API uses k anonymity
     query, tail = sha1password[:5], sha1password[5:]
     response = request_api(query)
     return response, tail
 
 
 def get_password_leaks(hashes, hash_of_interest):
+    # Creates List from each line in response
     hashes = (line.split(':') for line in hashes.text.splitlines())
+
     for h, count in hashes:
         if h == hash_of_interest:
             return int(count)
